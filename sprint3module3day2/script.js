3f44f17c7f21b8a3880ca090be6d0162c3252c24
derekjpeters <derek.j.peters@gmail.com>
2025-06-17 18:54:52 -0400

    updated module 3 day 2 of sprint 3

--- sprint3module3day2/script.js
+++ sprint3module3day2/script.js
@@ -1,1 +1,52 @@
-console.log("test")
+// setTimeout(() => {
+//     console.log("1. Start Cooking");
+// },2000);
+
+
+
+// setTimeout(() => {
+//     console.log("2. Dish is ready after 3 seconds");
+// }, 3000)
+
+// setTimeout(() => {
+//     console.log("3. Keep prepping for other dishes") 
+// },5000);
+
+// console.log("Start Timer")
+
+// setTimeout(() => {
+//     console.log("Almost There")
+// },2000);
+
+// setTimeout(() => {
+//     console.log("Times Up")
+// }, 4000);
+
+//window.onkeydown = () => console.log("User clicked item")
+
+// let count = 0
+// window.onclick = () => { count++; console.log('Button Clicked ' + count + ' times')}
+
+document.getElementById("launch").addEventListener("click", () => {
+    console.log("🚀 Rocket Launched")
+})
+
+document.addEventListener("keydown", () => {
+    if (event.key === "b" || "y" || "x") {
+        console.log("🔥 Ignition started...")
+
+        let countdown = 10;
+
+        function runCountDown() {
+            if (countdown > 0) {
+                console.log(`Countdown: ${countdown}`)
+                countdown--
+
+                setTimeout(runCountDown, 1000);
+            } else {
+                console.log("🚀 Liftoff!")
+            }
+        }
+        runCountDown();
+    }
+})
