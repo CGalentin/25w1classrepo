894f43ab16e09d7043851955a129634dc283a518
derekjpeters <derek.j.peters@gmail.com>
2025-07-01 17:49:21 -0400

    lesson 2 for DOM added

---
+++ sprint5module1day2/script.js
@@ -0,0 +1,10 @@
+const heading = document.getElementById('main-heading');
+const description = document.querySelector('.description');
+const changeTextBtn = document.getElementById('change-text-btn');
+const addItemBtn = document.getElementById('add-item-btn');
+const itemList = document.getElementById('item-list');
+const container = document.getElementById('new-element-container');
+const learnMoreLink = document.getElementById('learn-more-link');
+const sampleImage = document.getElementById('sample-image');
+const changeImageBtn = document.getElementById('change-image-btn');
+
