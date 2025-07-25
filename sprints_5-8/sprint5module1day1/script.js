a3d9ed64ba80aeabb84ccf3cbcd2d0d4436111e2
Chris Galentinemail <chrisgalentin.bloomtech@gmail.com>
2025-07-25 16:54:07 -0400

    "update from my work"

--- sprints_5-8/sprint5module1day1/script.js
+++ sprints_5-8/sprint5module1day1/script.js
@@ -1,42 +1,54 @@
 console.log(window)
 
-const heading = document.getElementById("main-heading");
+// const heading = document.getElementById("main-heading");
+const heading = document.getElementById("main-heading")
 
+// console.log(heading)
 console.log(heading)
 
-heading.textContent = "The DOM Has Changed"
 
-//document.getElementByID 
+// heading.textContent = "The DOM Has Changed"
+heading.textContent = "New Heading"
 
-const btn1 = document.getElementById("btn-1");
+// //document.getElementByID 
+
+// const btn1 = document.getElementById("btn-1");
+// console.log(btn1)
+const btn1 = document.getElementById("btn-1")
 console.log(btn1)
+// //.getElementsByClassName
+// //.getElementsByTagName
 
-//.getElementsByClassName
-//.getElementsByTagName
-
-const infoParagraphs = document.getElementsByClassName("info");
-
+// const infoParagraphs = document.getElementsByClassName("info");
+const infoParagraphs = document.getElementsByClassName("info")
 console.log(infoParagraphs);
 
-const allButtons = document.getElementsByTagName("button");
-
+// const allButtons = document.getElementsByTagName("button");
+const allButtons = document.getElementsByTagName("button")
 console.log(allButtons)
 
-//.querySelector()
+// //.querySelector()
 
-const firstInfo = document.querySelector(".info");
+// const firstInfo = document.querySelector(".info");
+const firstInfo = document.querySelector(".info")
 
 console.log(firstInfo)
 
+// const allInfo = document.querySelectorAll(".info")
 const allInfo = document.querySelectorAll(".info")
 
 console.log(allInfo)
 
+// //.forEach()
+// allInfo.forEach(function(para) {
+//     console.log(para.textContent)
+//     para.style.color="orange"
+// })
 
-//.forEach()
+// .forEach()
 
-allInfo.forEach(function(para) {
+allInfo.forEach(function(para){
     console.log(para.textContent)
-    para.style.color="orange"
+    para.style.color="blue"
 })
-
+    
